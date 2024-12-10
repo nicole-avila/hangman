ba860490540912cbddce32af06d6c44de805e01b
nicole-avila <114564720+nicole-avila@users.noreply.github.com>
2024-12-10 19:41:03 +0100

    adding README file

--- obj/Debug/net8.0/hangmanGame.AssemblyInfo.cs
+++ obj/Debug/net8.0/hangmanGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hangmanGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b592028b1cac2d69a8a4fce1ce9b3d015e34e572")]
 [assembly: System.Reflection.AssemblyProductAttribute("hangmanGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hangmanGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
