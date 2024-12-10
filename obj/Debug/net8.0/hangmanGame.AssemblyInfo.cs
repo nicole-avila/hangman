44e6fca3e6da3c3804178fecd79abdce7f58543c
nicole-avila <114564720+nicole-avila@users.noreply.github.com>
2024-12-10 19:56:27 +0100

    install NuGet packages: Spectre.Console, Newtonsoft.Json, FluentValidation

--- obj/Debug/net8.0/hangmanGame.AssemblyInfo.cs
+++ obj/Debug/net8.0/hangmanGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hangmanGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29cfce82ae9d847e1ebd327f4aa4688101e596be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2b7dff50f821eac0a420c064f6d217faef62ab7")]
 [assembly: System.Reflection.AssemblyProductAttribute("hangmanGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hangmanGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
